44973712988eaee1dc40b79b668fe085895376da
“Vladimir <me@vkalinichev.com>
2023-06-21 22:22:33 +0300

    noise wip

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
-const button = document.getElementById('start-button');
-const main = document.getElementById('main');
+const lookupTable = Array.from({length: 1e7}).map(() => Math.random() < 0.5 ? 0 : -1);
+let i =0;
 
-const animation = {
-  autoFullscreen: false,
-  started: false,
-
-  start() {
-    if (!animation.started) {
-      animation.started = true;
-      main.classList.add('animated');
-      main.requestFullscreen({navigationUI: 'hide'}).then(() => {
-        animation.autoFullscreen = true
-      });
-    }
-  },
-
-  stop() {
-    if (animation.started) {
-      animation.started = false;
-      main.classList.remove('animated');
-      button.classList.add('initialized');
-
-      if (animation.autoFullscreen && document.fullscreenElement === main) {
-        document.exitFullscreen();
-      }
-    }
+function lookup() {
+  ++i;
+  if (i>=lookupTable.length) {
+    i=0;
   }
+  return lookupTable[i];
 }
 
-button.addEventListener('click', () => animation.start());
-main.addEventListener('click', (event) => {
-  if (event.target === main) {
-    animation.stop();
-  }
-});
+const canvas = document.getElementById('canvas');
+const rect = canvas.getBoundingClientRect();
+const DPR = window.devicePixelRatio || 1;
+const width = rect.width * DPR;
+const height = rect.height * DPR;
 
-document.addEventListener('fullscreenchange', () => {
-  if (!document.fullscreenElement) {
-    animation.stop();
+function initCanvas(canvas) {
+  canvas.width = width;
+  canvas.height = height;
+
+  const context = canvas.getContext('2d', {alpha: false});
+  context.scale(DPR, DPR);
+  return context;
+}
+
+function render(source) {
+  context.drawImage(source, 0, 0);
+}
+
+const context = initCanvas(canvas);
+
+const IMAGE_COUNT = 16;
+let imageI = 0;
+let images = [];
+
+function noise() {
+  const i = imageI % IMAGE_COUNT;
+
+  if (!images[i]) {
+    const img = new Image();
+    const imageData = context.createImageData(width, height);
+    let buffer = new Uint32Array(imageData.data.buffer);
+    for (let i=0; i<buffer.length; i++) {
+      buffer[i] = lookup();
+    }
+    context.putImageData(imageData, 0, 0);
+    img.src = canvas.toDataURL();
+    images[i] = img;
+  } else {
+    context.clearRect(0, 0, width, height);
+    context.drawImage(images[i], 0, 0, width / 2, height / 2);
   }
-});
+
+  imageI++
+}
+
+function loop() {
+  noise();
+  // requestAnimationFrame(loop)
+  setTimeout(loop, 32);
+}
+
+loop();
